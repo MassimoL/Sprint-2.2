80907a7ee865f1d273ae4aa6453afcdc2ba79793
MassimoL <massimolucianoweb@gmail.com>
2023-12-13 14:17:50 +0100

    error message if field is not filled

--- js/checkout.js
+++ js/checkout.js
@@ -1,80 +1,59 @@
 // Exercise 6
 
-function validate(event) {
+function validate() {
+    let error = 0;
+    event.preventDefault(); // To avoid reloading the page when clicking SAVE and to display the console.log
 
-	let error = 0;	
+    let lettersOnly = /^[A-Za-z]+$/;
+    let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,3})+$/;
+    let numberFormat = /^\d{9}$/;
+    let passwordFormat = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 
-	let forms = document.querySelectorAll('.needs-validation');
+    // Get the input fields
+    let fName = document.getElementById("fName");
+    let fEmail = document.getElementById("fEmail");
+    let fAddress = document.getElementById("fAddress");
+    let fLastN = document.getElementById("fLastN");
+    let fPassword = document.getElementById("fPassword");
+    let fPhone = document.getElementById("fPhone");
 
-	var form = forms[0];
+    // Get the error elements
+    let errorName = document.getElementById("errorName");
+    let errorEmail = document.getElementById("errorEmail");
+    let errorAddress = document.getElementById("errorAddress");
+    let errorLastN = document.getElementById("errorLastN");
+    let errorPassword = document.getElementById("errorPassword");
+    let errorPhone = document.getElementById("errorPhone");
 
-	form.addEventListener('submit', event => {
-		if (!form.checkValidity()) {
-		  event.preventDefault()
-		  event.stopPropagation()
-		}
-  
-		form.classList.add('was-validated')
-	  }, false)
+    // Validate fields entered by the user:
+    (fName.value === "" || !lettersOnly.test(fName.value)) ? showError(fName, errorName) : clearError(fName, errorName);
+    (fEmail.value === "" || !emailFormat.test(fEmail.value)) ? showError(fEmail, errorEmail) : clearError(fEmail, errorEmail);
+    (fAddress.value === "") ? showError(fAddress, errorAddress) : clearError(fAddress, errorAddress);
+    (fLastN.value === "" || !lettersOnly.test(fLastN.value)) ? showError(fLastN, errorLastN) : clearError(fLastN, errorLastN);
+    (fPassword.value === "" || !passwordFormat.test(fPassword.value)) ? showError(fPassword, errorPassword) : clearError(fPassword, errorPassword);
+    (fPhone.value === "" || !numberFormat.test(fPhone.value)) ? showError(fPhone, errorPhone) : clearError(fPhone, errorPhone);
 
-	event.preventDefault(); // To avoid reloading the page when clicking SAVE and to display the console.log
-	
-	let lettersOnly = /^[A-Za-z]+$/
-    let emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{1,3})+$/
-    let numberFormat = /^\d{9}$/
-    let passwordFormat = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/
-	
-	// Get the input fields
-	let fName = document.getElementById("fName").value;
-	let fEmail = document.getElementById("fEmail").value;
-	let fAddress = document.getElementById("fAddress").value;
-	let fLastN = document.getElementById("fLastN").value;
-	let fPassword = document.getElementById("fPassword").value;
-	let fPhone = document.getElementById("fPhone").value;
-
-	// Get the error elements
-	let errorName = document.getElementById("errorName");
-	let errorEmail = document.getElementById("errorEmail");
-	let errorAddress = document.getElementById("errorAddress");
-	let errorLastN = document.getElementById("errorLastN");
-	let errorPassword = document.getElementById("errorPassword");
-	let errorPhone = document.getElementById("errorPhone");
-	
-	// Validate fields entered by the user: 
-	(fName === "" || !lettersOnly.test(fName)) ? showError(errorName) : clearError(errorName);
-	(fEmail === "" || !emailFormat.test(fEmail)) ? showError(errorEmail) : clearError(errorEmail);
-	(fAddress === "") ? showError(errorAddress) : clearError(errorAddress);
-	(fLastN === "" || !lettersOnly.test(fLastN)) ? showError(errorLastN) : clearError(errorLastN);
-	(fPassword === "" || !passwordFormat.test(fPassword)) ? showError(errorPassword) : clearError(errorPassword);
-	(fPhone === "" || !numberFormat.test(fPhone)) ? showError(errorPhone) : clearError(errorPhone);
-
-	  // Add an event listener to the form to capture the input:
-	let form = document.querySelector("form"); 
-	form.addEventListener("submit", validate);
-
-	function showError(element) {
-        error++;
-        element.style.display = 'block';  
-        element.classList.add('is-invalid'); 
-	}
-
-	function clearError(element) {
-        error--;
-        element.style.display = 'none';  
-        element.classList.remove('is-invalid'); 
+    if (error > 0) {
+        console.log("Error");
+    } else {
+        console.log("OK" +
+            "\nName: " + fName.value +
+            "\nEmail: " + fEmail.value +
+            "\nAddress: " + fAddress.value +
+            "\nLast Name: " + fLastN.value +
+            "\nPassword: " + fPassword.value +
+            "\nPhone: " + fPhone.value);
     }
 
-	if(error > 0){
-		event.preventDefault();
-		console.log("Error");
-	}else{
-		console.log("OK" +
-		"\nName: " + fName + 
-		"\nEmail: " + fEmail + 
-		"\nAddress: " + fAddress +
-		"\nLast Name: " + fLastN + 
-		"\nPassword: " + fPassword +
-		"\nPhone: " + fPhone);
-	}
+    function showError(field, element) {
+        error++;
+        element.style.display = 'block';
+        field.classList.add('is-invalid');
+    }
 
+    function clearError(field, element) {
+        error--;
+        element.style.display = 'none';
+        field.classList.remove('is-invalid');
+    }
 }
